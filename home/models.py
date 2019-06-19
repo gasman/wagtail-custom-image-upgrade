23588cf619f37bcf21b548f0e46c96cba23eb836
Matt Westcott <matt@west.co.tt>
2019-06-19 12:20:44 +0100

    upgrade to Wagtail 1.10

--- home/models.py
+++ home/models.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
-from django.db.models.signals import post_delete
-from django.dispatch import receiver
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailimages.models import Image, AbstractImage, AbstractRendition
@@ -28,15 +26,3 @@ class CustomRendition(AbstractRendition):
         unique_together = (
             ('image', 'filter_spec', 'focal_point_key'),
         )
-
-
-# Delete the source image file when an image is deleted
-@receiver(post_delete, sender=CustomImage)
-def image_delete(sender, instance, **kwargs):
-    instance.file.delete(False)
-
-
-# Delete the rendition image file when a rendition is deleted
-@receiver(post_delete, sender=CustomRendition)
-def rendition_delete(sender, instance, **kwargs):
-    instance.file.delete(False)
